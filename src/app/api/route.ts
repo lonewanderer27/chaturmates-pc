ba535b9e7fc55ab6e780153ee850eb7b462e9ca7
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-12-01 02:51:03 +0800

    Update response status text in GET and POST functions

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,11 @@
 export async function GET(request: Request) {
-  return new Response("Hello klasmeyt!", { status: 200 });
+  return new Response("Hello klasmeyt!", { status: 200, statusText: "OK" });
 }
 
 export async function POST(request: Request) {
   const { name } = await request.json();
-  return new Response(`Hello klasmeyt ${name}!`, { status: 200 });
+  return new Response(`Hello klasmeyt ${name}!`, {
+    status: 200,
+    statusText: "OK",
+  });
 }
