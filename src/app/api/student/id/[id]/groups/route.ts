109053c2cbbeec6e7b1289850debbb2015662237
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-12-03 20:28:20 +0800

    Add POST route to create a new group for a student

--- src/app/api/student/id/[id]/groups/route.ts
+++ src/app/api/student/id/[id]/groups/route.ts
@@ -1,4 +1,113 @@
 import { client } from "@/client";
+import { newGroup } from "@/validators/group";
+import Joi from "joi";
+
+export async function POST(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  // fetch the student id from the url
+  console.log(params);
+  const { id } = params;
+
+  // if the student id is not present, return an error
+  if (!id) {
+    return new Response(
+      JSON.stringify({ error: "Missing student id", success: false }),
+      {
+        status: 400,
+        statusText: "Bad Request",
+      }
+    );
+  }
+
+  // verify if the student exists
+  const student = await client
+    .from("students")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  // if the student is not found, return an error
+  if (!student) {
+    return new Response(
+      JSON.stringify({ error: "Student not found", success: false }),
+      {
+        status: 404,
+        statusText: "Not Found",
+      }
+    );
+  }
+
+  // this student id is going to be our first group member and the admin\
+  // validate the following info
+  const { error, value } = newGroup.validate(await request.json());
+
+  // if there is an error, return an error
+  if (error) {
+    return new Response(
+      JSON.stringify({ error: error.message, success: false }),
+      {
+        status: 400,
+        statusText: "Bad Request",
+      }
+    );
+  }
+
+  // if there is no error, create a new group
+  const group = await client.from("groups").insert([
+    value
+  ]).select("*").single();
+
+  // if the group is not created, return an error
+  if (!group) {
+    return new Response(
+      JSON.stringify({ error: "Group not created", success: false }),
+      {
+        status: 500,
+        statusText: "Internal Server Error",
+      }
+    );
+  }
+
+  // add the student as the first group member and the admin
+  const groupMember = await client
+    .from("group_members")
+    .insert([
+      {
+        group_id: group.data!.id,
+        student_id: id,
+        profile_id: student.data!.profile_id,
+        approved: true,
+        admin: true,
+      }
+    ])
+    .select("*").single();
+
+  // if the group member is not added, return an error
+  if (!groupMember) {
+    return new Response(
+      JSON.stringify({ error: "Group member not added", success: false }),
+      {
+        status: 500,
+        statusText: "Internal Server Error",
+      }
+    );
+  }
+
+  // return a success message
+  return new Response(
+    JSON.stringify({
+      message: "Group created successfully",
+      success: true,
+      data: group.data,
+    }),
+    {
+      status: 201,
+      statusText: "Created",
+    }
+  )
+}
 
 export async function GET(
   request: Request,
